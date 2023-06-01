927ba45097e9cc5a9cee3529d905e6aee036770c
SviatoslavPaseka <paseka.syat@gmail.com>
2023-06-01 15:55:40 +0300

    get back to start

--- src/test/java/org/demo/SelenoidTest.java
+++ src/test/java/org/demo/SelenoidTest.java
@@ -4,22 +4,15 @@ import java.lang.invoke.MethodHandles;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SelenoidTest {
-    @BeforeTest
-    public void intro(){
-        System.out.println("Starting test");
-    }
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public String url = "https://rozetka.com.ua/ua/";
@@ -42,11 +35,10 @@ public class SelenoidTest {
         driver.manage().window().maximize();
         driver.get(url);
 
-        WebElement searchField = driver.findElement(By.xpath("//input[@name='search']"));
-        System.out.println("Title: " + searchField.getAttribute("placeholder"));
-
-        Assert.assertTrue(searchField.isDisplayed(),  "Search field are not displayed");
-//        Assert.assertEquals(searchField.getAttribute("placeholder"),"Я шукаю...",  "Text in search field is wrong");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Інтернет-магазин ROZETKA™: офіційний сайт найпопулярнішого онлайн-гіпермаркету " +
+                "в Україні", "Title text not equals as expected");
+        LOGGER.info("All ok.");
         driver.quit();
     }
 }
